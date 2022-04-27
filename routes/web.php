ae8ac10ee455efa980c46aed54052c97995b7bb7
MalvinMM <juniormalvinn@gmail.com>
2022-04-27 16:53:54 +0700

    finishing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MemberController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('landing');
+Route::get('/', [MemberController::class, 'landing']);
+Route::get('/about',[MemberController::class, 'about']);
+Route::get('/service', [MemberController::class, 'services']);
+
+Route::prefix('dashboard')->group(function(){
+    Route::get('/',[MemberController::class, 'dashboard']);
+
+    Route::get('/addMember', [MemberController::class, 'form']);
+    Route::post('/addMember', [MemberController::class, 'store']);
+
+    Route::prefix('member')->group(function(){
+        
+        Route::get('/edit/{id}',[MemberController::class, 'edit']);
+        Route::post('/edit/{id}', [MemberController::class, 'update']);
+
+        Route::get('/del/{id}', [MemberController::class, 'delete']);         
+    });
+
 });
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/service', function () {
-    return view('service');
-});
+
