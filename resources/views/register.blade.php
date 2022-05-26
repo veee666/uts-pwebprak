c6eef759f40cb2cc54b407bfb2d5853e569a2532
veee666 <annisarahmaputri04@gmail.com>
2022-05-26 17:44:16 +0700

    navbar-changes

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,10 +4,10 @@
 <link href="/css/dashboard.css" rel="stylesheet">
 
 @section('content')
-<div class="container-service">
-    <div class="main-about">
-        <div class="images-about" style= "margin-bottom: 100px;">
-        <section class="banner-about">
+<div class="container-register">
+    <div class="main-register">
+        <div class="images-register" style= "margin-bottom: 100px;">
+        <section class="banner-register">
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom" style="margin-top:-100px">
                     <h1 class="h2">Enter Member Credentials</h1>
@@ -20,7 +20,7 @@
                         <input type="text" placeholder="Enter Name" class="form-control" id="nama" aria-describedby="nama" name="namaMember">
                     </div>
                     <div class="mb-3">
-                        <label for="psw"><b>Password</b></label>
+                        <label for="psw" class="form-label">Password</label>
                         <input type="password" placeholder="Enter Password" class="form-control" name="password" id="psw" required>
                     </div>
                     <div class="mb-3">
